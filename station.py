e24b2e79823843455aaabe3cc540124ca71f8427
aboutxyz <haishangmuxucao@dlmu.edu.cn>
2017-01-04 21:31:17 +0800

    update

--- station.py
+++ station.py
@@ -7,20 +7,22 @@ repls = ('hello', 'goodbye'), ('world', 'earth')
 s = 'hello, world'
 reduce(lambda a, kv: a.replace(*kv), repls, s)
 '''
-stationlist = []
-with open('stationname.js','r') as f:
-    patten = re.compile(r"@([a-z]*)\|(.*?)\|([A-Z]*)\|([a-z]*)\|([a-z]*)\|([0-9]*)")
-    resu= f.read()
-    result = patten.findall(resu)
-    # connection = pymongo.MongoClient("localhost", 27017)
-    # db = connection.train
-    # collection = db.station
-    for i in result:
-        repls = (u'东', '',1), (u'南', '',1), (u'西', '',1), (u'北', '',1)
-        x = i[1].decode('gbk')[:-1] + reduce(lambda a, kv: a.replace(*kv), repls, i[1].decode('gbk')[-1])
-        stationlist.append(x)
-    for i in stationlist:
-        print i
-        # if i[1].decode('gbk')
-        # user = {"name": i[1].decode('gbk'), "code": i[2]}
-        # collection.insert(user)
+sname = []
+connection = pymongo.MongoClient("localhost", 27017)
+db = connection.train
+collection = db.cityname
+collection1 = db.stationname
+collection2 = db.upstationname
+for i in collection1.find():
+    sname.append(i['name'])
+for i in collection.find():
+    i.setdefault('sub', 'none')
+    i.setdefault('secsub', 'none')
+    for j in sname:
+        if any(x in j for x in [i['city'], i['sub'], i['secsub']]):
+            print j,i
+            i['station'] = j
+            user = {"city": i['city'], "sub": i['sub'], "secsub": i['secsub'], "station": i['station']}
+            collection2.insert(user)
+
+
