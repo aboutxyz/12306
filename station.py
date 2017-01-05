b2e1877ae0987c626e512fc0c16c95cb67b5905e
aboutxyz <haishangmuxucao@dlmu.edu.cn>
2017-01-05 22:25:57 +0800

    update1

--- station.py
+++ station.py
@@ -6,6 +6,7 @@ test=[]
 repls = ('hello', 'goodbye'), ('world', 'earth')
 s = 'hello, world'
 reduce(lambda a, kv: a.replace(*kv), repls, s)
+repls = (u'东', ''), (u'南', ''), (u'西', ''), (u'北', '')
 '''
 sname = []
 connection = pymongo.MongoClient("localhost", 27017)
@@ -16,13 +17,17 @@ collection2 = db.upstationname
 for i in collection1.find():
     sname.append(i['name'])
 for i in collection.find():
+    stationlist = []
     i.setdefault('sub', 'none')
     i.setdefault('secsub', 'none')
-    for j in sname:
-        if any(x in j for x in [i['city'], i['sub'], i['secsub']]):
+    for j in sname:# sname 是站名
+        if any(x.replace(u'县', '').startswith(j) for x in [i['city'], i['sub'], i['secsub']]):
             print j,i
-            i['station'] = j
-            user = {"city": i['city'], "sub": i['sub'], "secsub": i['secsub'], "station": i['station']}
-            collection2.insert(user)
+            stationlist.append(j)
+            for y in sname[:]:
+                if j in y:
+                    stationlist.append(y)
+    user = {"city": i['city'], "sub": i['sub'], "secsub": i['secsub'], "station": list(set(stationlist))}
+    collection2.insert(user)
 
 
